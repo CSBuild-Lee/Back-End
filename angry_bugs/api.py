7bd7e54e814f485001e1cd2e2235247b90650f85
Adan <adanrodriguezevo@gmail.com>
2020-02-06 16:49:38 -0600

    moved folders, testing

--- angry_bugs/api.py
+++ angry_bugs/api.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers, viewsets
 from .models import PersonalNote
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+# from pusher import Pusher
+from django.http import JsonResponse
+from decouple import config
+from django.contrib.auth.models import User 
+from rest_framework.decorators import api_view
+import json
+from .models import Room, room_dict, Player
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
     # Inner class nested inside PersonalNoteSerializer
@@ -25,7 +34,64 @@ class PersonalNoteViewSet(viewsets.ModelViewSet):
                 return PersonalNote.objects.none()
             else:
                 return PersonalNote.objects.filter(user=user)
+                
 
+@csrf_exempt
+@api_view(["GET"])
+def initialize(request):
+
+    user = request.user
+
+    # initialize rooms
+    for i in range(1,101):
+        r = Room()
+        r.id = 1
+        r.value = room_dict[room_type]
+        r.save()
+
+    player = user.player
+    player_id = player.id
+    uuid = player.uuid
+    room = player.room()
+
+    rooms = Room.objects.all()
+    return JsonResponse({rooms, {'value': player.calories, 'killed': player.num_rooms_eaten}, {'room_id':1}})
+
+
+# @csrf_exempt
+@api_view(["POST"])
+def move(request):
+
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    data = json.loads(request.body)
+    direction = data['direction']
+    room = player.room()
+    nextRoomID = None
+    if direction == "n":
+        nextRoomID = room.n_to
+    elif direction == "s":
+        nextRoomID = room.s_to
+    elif direction == "e":
+        nextRoomID = room.e_to
+    elif direction == "w":
+        nextRoomID = room.w_to
+    if nextRoomID is not None and nextRoomID > 0:
+        nextRoom = Room.objects.get(id=nextRoomID)
+        player.currentRoom=nextRoomID
+        player.save()
+        players = nextRoom.playerNames(player_id)
+        currentPlayerUUIDs = room.playerUUIDs(player_id)
+        nextPlayerUUIDs = nextRoom.playerUUIDs(player_id)
+        for p_uuid in currentPlayerUUIDs:
+            pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
+        for p_uuid in nextPlayerUUIDs:
+            pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
+        return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
+    else:
+        players = room.playerNames(player_id)
+        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
 
--- angry_bugs/models.py
+++ angry_bugs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from uuid import uuid4
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
+import random
 
 # Create your models here.
 class Note(models.Model):
@@ -12,5 +13,88 @@ class PersonalNote(Note):   # Inherits from Note!
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
+
+
+room_dict = {
+    'Lettuce' : 50,
+    'Cucumber': 160,
+    'Tomato': 180,
+    'Eggplant': 200,
+    'Carrot': 250,
+    'Pepper': 300,
+    'Broccoli': 500,
+    'Corn': 850,
+    'Potato': 1630,
+    'Yam': 1770,
+}
+
+class Room(models.Model):
+    # id?
+    room_type = models.CharField(max_length = 600 , default = random.choice(list(room_dict.keys())))
+    # print(room_dict)
+    # print(room_type)
+    # print(dir(room_type))
+    value = models.IntegerField(default = 0)
+    isDead = models.BooleanField(default=False)
+
+    # def __str__(self)
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    row = models.IntegerField(default = 0)
+    col = models.IntegerField(default = 0)
+    calories = models.IntegerField(default = 0)
+    room_id = models.IntegerField(default = 1)
+    num_rooms_eaten = models.IntegerField(default = 0)
+    # changing room where player spawns to eaten without updating calories
+    # so that room will only show player and not vegetable
+    # Room().isDead = True
+    
+    def eat(self, direction):
+        self.__move__(direction)
+        self.__eat_current_loc__()
+
+    def __move__(self, direction):
+        error = 'error, can not move in that direction'
+
+        if direction == 'Up':
+            # check if player can move
+            if self.row == 0:
+                return error
+            # move player
+            else:
+                self.row -= 1
+        
+        if direction == 'Down':
+            if self.row == 9:
+                return error
+            else:
+                self.row += 1
+        
+        if direction == 'Right':
+            if self.col == 9:
+                return error
+            else:
+                self.col += 1
+        
+        if direction == 'Left':
+            if self.col == 0:
+                return error
+            else:
+                self.col -= 1
+
+        self.room_id = (self.row*10) + (self.col+1)
+    
+    def __eat_current_loc__(self):
+        # update player calories
+        self.calories += self.Room().value
+        # change room to eaten
+        self.Room().isDead = True
+        self.num_rooms_eaten += 1
+
+    def Room(self):
+        return Room.objects.get(id=self.room_id)
+# objects to return
 
 
