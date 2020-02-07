6e1f28347c25a24fec7d39c3369286896e3b8126
Adan <adanrodriguezevo@gmail.com>
2020-02-07 12:12:25 -0600

    made some changes

--- angry_bugs/api.py
+++ angry_bugs/api.py
@@ -12,6 +12,7 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from uuid import uuid4
 import random
+from django.core.serializers.json import DjangoJSONEncoder
 
 # class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
 #     # Inner class nested inside PersonalNoteSerializer
@@ -60,49 +61,14 @@ def initialize(request):
     # room = player.room()
 
     def get_queryset(self):
-        user = self.request.user
-
-    rooms = [room for room in Room.objects.all()]
+    rooms = Room.objects.all().values_list('id','room_type','value','isDead')
+    # prices = Price.objects.filter(product=product).values_list('price','valid_from')
+    # rooms = [room for room in Room.objects.all()]
     print(rooms)
-    print(type(rooms))
-    return JsonResponse({'rooms': str(rooms), 'value': player.calories, 'killed': player.num_rooms_eaten, 'room_id':1})
-# @csrf_exempt
-# @api_view(["POST"])
-# @permission_classes((permissions.AllowAny,))
-# def move(request):
+    rooms_json = json.dumps(list(rooms), cls=DjangoJSONEncoder)
+    print(rooms_json)
+    return JsonResponse({'rooms': rooms_json, 'value': player.calories, 'killed': player.num_rooms_eaten, 'room_id':1})
 
-#     player = request.user.player
-#     player_id = player.id
-#     player_uuid = player.uuid
-#     data = json.loads(request.body)
-#     direction = data['direction']
-#     room = player.room()
-#     nextRoomID = None
-#     if direction == "n":
-#         nextRoomID = room.n_to
-#     elif direction == "s":
-#         nextRoomID = room.s_to
-#     elif direction == "e":
-#         nextRoomID = room.e_to
-#     elif direction == "w":
-#         nextRoomID = room.w_to
-#     if nextRoomID is not None and nextRoomID > 0:
-#         nextRoom = Room.objects.get(id=nextRoomID)
-#         player.currentRoom=nextRoomID
-#         player.save()
-#         players = nextRoom.playerNames(player_id)
-#         currentPlayerUUIDs = room.playerUUIDs(player_id)
-#         nextPlayerUUIDs = nextRoom.playerUUIDs(player_id)
-#         for p_uuid in currentPlayerUUIDs:
-#             pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
-#         for p_uuid in nextPlayerUUIDs:
-#             pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
-#         return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
-#     else:
-#         players = room.playerNames(player_id)
-#         return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
-
-# @csrf_exempt
 @api_view(["POST"])
 @permission_classes((permissions.AllowAny,))
 def move(request):
